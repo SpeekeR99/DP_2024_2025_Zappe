1fb9ed80404970fd311d6c4144639746d76618df
SpeekeR <DominikZappe1999@seznam.cz>
2025-02-18 16:31:04 +0100

    Our data fit onto the IForest and different IForest params testing

--- AI/src/iforest.py
+++ AI/src/iforest.py
@@ -1,40 +1,44 @@
-from sklearn.ensemble import IsolationForest
-from sklearn.model_selection import train_test_split
-from sklearn.datasets import load_iris
+import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.ensemble import IsolationForest
 
-iris = load_iris()
-X = iris.data
-y = iris.target
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+# Load the data
+filepath = "../data/lobster_best_prices_only.csv"
+data = pd.read_csv(filepath)
+data_numpy = data.to_numpy()
 
 # initialize and fit the model
-clf = IsolationForest(contamination=0.1)
-clf.fit(X_train)
+clf = IsolationForest(contamination=0.01, n_estimators=1000, max_samples=1.0, max_features=1.0)
+clf.fit(data_numpy)
 
 # predict the anomalies in the data
-y_pred_train = clf.predict(X_train)
-y_pred_test = clf.predict(X_test)
-print(y_pred_train)
-print(y_pred_test)
+y_pred_train = clf.predict(data_numpy)
 
-def create_scatter_plots(X1, y1, title1, X2, y2, title2):
-    fig, axes = plt.subplots(1, 2, figsize=(12, 6))
+# plot the anomalies
+fig, axes = plt.subplots(2, 2, figsize=(20, 10))
 
-    # Scatter plot for the first set of data
-    axes[0].scatter(X1[y1==1, 0], X1[y1==1, 1], color='green', label='Normal')
-    axes[0].scatter(X1[y1==-1, 0], X1[y1==-1, 1], color='red', label='Anomaly')
-    axes[0].set_title(title1)
-    axes[0].legend()
+axes[0][0].scatter(data_numpy[y_pred_train==1, 0], data_numpy[y_pred_train==1, 1], color="green", label="Normal Ask")
+axes[0][0].scatter(data_numpy[y_pred_train==-1, 0], data_numpy[y_pred_train==-1, 1], color="red", label="Anomaly Ask")
+axes[0][0].set_title("Ask Price")
+axes[0][0].legend()
+axes[0][0].grid()
 
-    # Scatter plot for the second set of data
-    axes[1].scatter(X2[y2==1, 0], X2[y2==1, 1], color='green', label='Normal')
-    axes[1].scatter(X2[y2==-1, 0], X2[y2==-1, 1], color='red', label='Anomaly')
-    axes[1].set_title(title2)
-    axes[1].legend()
+axes[0][1].scatter(data_numpy[y_pred_train==1, 0], data_numpy[y_pred_train==1, 3], color="green", label="Normal Bid")
+axes[0][1].scatter(data_numpy[y_pred_train==-1, 0], data_numpy[y_pred_train==-1, 3], color="red", label="Anomaly Bid")
+axes[0][1].set_title("Bid Price")
+axes[0][1].legend()
+axes[0][1].grid()
 
-    plt.tight_layout()
-    plt.show()
+axes[1][0].scatter(data_numpy[y_pred_train==1, 0], data_numpy[y_pred_train==1, 5], color="green", label="Normal Imbalance Index")
+axes[1][0].scatter(data_numpy[y_pred_train==-1, 0], data_numpy[y_pred_train==-1, 5], color="red", label="Anomaly Imbalance Index")
+axes[1][0].set_title("Imbalance Index")
+axes[1][0].legend()
+axes[1][0].grid()
 
-# scatter plots
-create_scatter_plots(X_train, y_pred_train, 'Training Data', X_test, y_pred_test, 'Test Data')
+axes[1][1].scatter(data_numpy[y_pred_train==1, 0], data_numpy[y_pred_train==1, 6], color="green", label="Normal Frequency")
+axes[1][1].scatter(data_numpy[y_pred_train==-1, 0], data_numpy[y_pred_train==-1, 6], color="red", label="Anomaly Frequency")
+axes[1][1].set_title("Frequency of incoming messages")
+axes[1][1].legend()
+axes[1][1].grid()
+
+plt.show()
