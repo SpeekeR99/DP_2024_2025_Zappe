febae5ff881b6ddcd6f4fa0f469e7804f524f48d
SpeekeR <DominikZappe1999@seznam.cz>
2025-03-09 15:38:37 +0100

    One class SVM working on our data

--- AI/src/ocsvm.py
+++ AI/src/ocsvm.py
@@ -1,35 +1,68 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.svm import OneClassSVM
-from sklearn.datasets import load_iris
-from sklearn.model_selection import train_test_split
 
-iris = load_iris()
-X = iris.data
-y = iris.target
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+# Load the data
+# filepath = "../data/lobster_best_prices_only.csv"
+filepath = "../../A7/pokus_lobsteru.csv"
+data = pd.read_csv(filepath)
+# data = data[["Time", "Ask Price 1", "Bid Price 1", "Imbalance Index", "Frequency of Incoming Messages", "Cancellations Rate"]]
+data = data.head(10000)
 
-clf_svm = OneClassSVM(kernel="rbf", degree=3, gamma=0.1, nu=0.01)
-y_predict_train = clf_svm.fit_predict(X_train)
-y_predict_test = clf_svm.predict(X_test)
+# Get the indices of the columns
+try:
+    time_idx = data.columns.get_loc("Time")
+    ask_price_idx = data.columns.get_loc("Ask Price 1")
+    bid_price_idx = data.columns.get_loc("Bid Price 1")
+    imbalance_idx = data.columns.get_loc("Imbalance Index")
+    frequency_idx = data.columns.get_loc("Frequency of Incoming Messages")
+    cancellation_rate_idx = data.columns.get_loc("Cancellations Rate")
+except KeyError as e:
+    print(f"The input file does not contain the necessary column {e}.")
+    exit(1)
 
-svm_predict_train = pd.Series(y_predict_train).replace([-1,1],[1,0])
-svm_anomalies_train = X_train[svm_predict_train == 1]
-svm_predict_test = pd.Series(y_predict_test).replace([-1,1],[1,0])
-svm_anomalies_test = X_test[svm_predict_test == 1]
+# Fill NaN values with the mean of the column
+# data.fillna(data.mean(), inplace=True)
+# data_numpy = data.to_numpy()
+data_numpy = data.dropna().to_numpy()
+mid_price = (data_numpy[:, ask_price_idx] + data_numpy[:, bid_price_idx]) / 2
 
-# Plot
-fig, axes = plt.subplots(1, 2, figsize=(12, 6))
+# Initialize and fit the model
+model = OneClassSVM(kernel="rbf")
+model.fit(data_numpy)
 
-axes[0].scatter(X_train[:, 0], X_train[:, 1], color='green', label='Normal')
-axes[0].scatter(svm_anomalies_train[:, 0], svm_anomalies_train[:, 1], color='red', label='Anomaly')
-axes[0].set_title('Training Data')
-axes[0].legend()
+# Predict the anomalies in the data
+y_pred = model.predict(data_numpy)
+y_scores = model.score_samples(data_numpy)
+y_scores_norm = (y_scores - y_scores.min()) / (y_scores.max() - y_scores.min())
+anomaly_proba = 1 - y_scores_norm  # The lower the original score, the higher "certainty" it is an anomaly
 
-axes[1].scatter(X_test[:, 0], X_test[:, 1], color='green', label='Normal')
-axes[1].scatter(svm_anomalies_test[:, 0], svm_anomalies_test[:, 1], color='red', label='Anomaly')
-axes[1].set_title('Test Data')
-axes[1].legend()
+# Plot the anomalies
+fig, axes = plt.subplots(2, 2, figsize=(20, 10))
+fig.suptitle("One-Class SVM")
 
-plt.tight_layout()
+axes[0][0].scatter(data_numpy[:, time_idx], mid_price, color="green", label="Normal Mid Price")
+axes[0][0].scatter(data_numpy[y_pred == -1, time_idx], mid_price[y_pred == -1], color="red", alpha=anomaly_proba[y_pred == -1], label="Anomaly Mid Price")
+axes[0][0].set_title("Mid Price")
+axes[0][0].legend()
+axes[0][0].grid()
+
+axes[0][1].scatter(data_numpy[:, time_idx], data_numpy[:, imbalance_idx], color="green", label="Normal Imbalance Index")
+axes[0][1].scatter(data_numpy[y_pred == -1, time_idx], data_numpy[y_pred == -1, imbalance_idx], color="red", alpha=anomaly_proba[y_pred == -1], label="Anomaly Imbalance Index")
+axes[0][1].set_title("Imbalance Index")
+axes[0][1].legend()
+axes[0][1].grid()
+
+axes[1][0].scatter(data_numpy[:, time_idx], data_numpy[:, frequency_idx], color="green", label="Normal Frequency")
+axes[1][0].scatter(data_numpy[y_pred == -1, time_idx], data_numpy[y_pred == -1, frequency_idx], color="red", alpha=anomaly_proba[y_pred == -1], label="Anomaly Frequency")
+axes[1][0].set_title("Frequency of incoming messages")
+axes[1][0].legend()
+axes[1][0].grid()
+
+axes[1][1].scatter(data_numpy[:, time_idx], data_numpy[:, cancellation_rate_idx], color="green", label="Normal Cancellations Rate")
+axes[1][1].scatter(data_numpy[y_pred == -1, time_idx], data_numpy[y_pred == -1, cancellation_rate_idx], color="red", alpha=anomaly_proba[y_pred == -1], label="Anomaly Cancellations Rate")
+axes[1][1].set_title("Cancellations Rate")
+axes[1][1].legend()
+axes[1][1].grid()
+
 plt.show()
