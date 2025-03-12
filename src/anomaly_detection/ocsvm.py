bf2da757933c02285aaf2fb49c0b60e0444f61e5
SpeekeR <DominikZappe1999@seznam.cz>
2025-03-12 23:21:15 +0100

    One-Class SVM results on the first 100k timestamps (bad results so far)

--- src/anomaly_detection/ocsvm.py
+++ src/anomaly_detection/ocsvm.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -7,22 +8,36 @@ from sklearn.svm import OneClassSVM
 DATE = "20191202"
 MARKET_SEGMENT_ID = "688"
 SECURITY_ID = "4128839"
-filepath = f"data/{DATE}_{MARKET_SEGMENT_ID}_{SECURITY_ID}_lobster_augmented.csv"
-data = pd.read_csv(filepath)
-# data = data[["Time", "Ask Price 1", "Bid Price 1", "Imbalance Index", "Frequency of Incoming Messages", "Cancellations Rate"]]
-data = data.head(100000)
 
-# Get the indices of the columns
+filepath = f"data/{DATE}_{MARKET_SEGMENT_ID}_{SECURITY_ID}_lobster_augmented.csv"
+if not os.path.exists(f"img/anomaly_detections/{DATE}_{MARKET_SEGMENT_ID}_{SECURITY_ID}"):
+    os.makedirs(f"img/anomaly_detections/{DATE}_{MARKET_SEGMENT_ID}_{SECURITY_ID}")
+data = pd.read_csv(filepath)
+
+# Keep only the "relevant" features
 try:
-    time_idx = data.columns.get_loc("Time")
-    ask_price_idx = data.columns.get_loc("Ask Price 1")
-    bid_price_idx = data.columns.get_loc("Bid Price 1")
-    imbalance_idx = data.columns.get_loc("Imbalance Index")
-    frequency_idx = data.columns.get_loc("Frequency of Incoming Messages")
-    cancellation_rate_idx = data.columns.get_loc("Cancellations Rate")
+    data = data[["Time", "Ask Price 1", "Ask Volume 1", "Bid Price 1", "Bid Volume 1", "Imbalance Index", "Frequency of Incoming Messages", "Cancellations Rate", "High Quoting Activity", "Unbalanced Quoting", "Low Execution Probability", "Trades Oppose Quotes", "Cancels Oppose Trades"]]
 except KeyError as e:
     print(f"The input file does not contain the necessary column {e}.")
     exit(1)
+# Take smaller subset of the data (for local computer speed purposes)
+data = data.head(100000)
+
+# Get the indices of the columns
+time_idx = data.columns.get_loc("Time")
+ask_price_idx = data.columns.get_loc("Ask Price 1")
+ask_volume_idx = data.columns.get_loc("Ask Volume 1")
+bid_price_idx = data.columns.get_loc("Bid Price 1")
+bid_volume_idx = data.columns.get_loc("Bid Volume 1")
+imbalance_idx = data.columns.get_loc("Imbalance Index")
+frequency_idx = data.columns.get_loc("Frequency of Incoming Messages")
+cancellation_rate_idx = data.columns.get_loc("Cancellations Rate")
+high_quoting_activity_idx = data.columns.get_loc("High Quoting Activity")
+unbalanced_quoting_idx = data.columns.get_loc("Unbalanced Quoting")
+low_execution_probability_idx = data.columns.get_loc("Low Execution Probability")
+trades_oppose_quotes_idx = data.columns.get_loc("Trades Oppose Quotes")
+cancels_oppose_trades_idx = data.columns.get_loc("Cancels Oppose Trades")
+indcs = [ask_price_idx, ask_volume_idx, bid_price_idx, bid_volume_idx, imbalance_idx, frequency_idx, cancellation_rate_idx, high_quoting_activity_idx, unbalanced_quoting_idx, low_execution_probability_idx, trades_oppose_quotes_idx, cancels_oppose_trades_idx]
 
 # Fill NaN values with the mean of the column
 # data.fillna(data.mean(), inplace=True)
@@ -44,31 +59,17 @@ anomaly_proba = 1 - y_scores_norm  # The lower the original score, the higher "c
 # anomaly_proba = np.where(anomaly_proba > thresh, anomaly_proba, 0)
 
 # Plot the anomalies
-fig, axes = plt.subplots(2, 2, figsize=(20, 10))
-fig.suptitle("One-Class SVM")
+for index in indcs:
+    fig = plt.figure(figsize=(20, 10))
+    fig.suptitle("One-Class SVM")
 
-axes[0][0].scatter(data_numpy[:, time_idx], mid_price, color="green", label="Normal Mid Price")
-axes[0][0].scatter(data_numpy[y_pred == -1, time_idx], mid_price[y_pred == -1], color="red", alpha=anomaly_proba[y_pred == -1], label="Anomaly Mid Price")
-axes[0][0].set_title("Mid Price")
-axes[0][0].legend()
-axes[0][0].grid()
+    plt.scatter(data_numpy[:, time_idx], data_numpy[:, index], color="green", label="Normal")
+    plt.scatter(data_numpy[y_pred == -1, time_idx], data_numpy[y_pred == -1, index], color="red", alpha=anomaly_proba[y_pred == -1], label="Anomaly")
 
-axes[0][1].scatter(data_numpy[:, time_idx], data_numpy[:, imbalance_idx], color="green", label="Normal Imbalance Index")
-axes[0][1].scatter(data_numpy[y_pred == -1, time_idx], data_numpy[y_pred == -1, imbalance_idx], color="red", alpha=anomaly_proba[y_pred == -1], label="Anomaly Imbalance Index")
-axes[0][1].set_title("Imbalance Index")
-axes[0][1].legend()
-axes[0][1].grid()
+    plt.title(data.columns[index])
 
-axes[1][0].scatter(data_numpy[:, time_idx], data_numpy[:, frequency_idx], color="green", label="Normal Frequency")
-axes[1][0].scatter(data_numpy[y_pred == -1, time_idx], data_numpy[y_pred == -1, frequency_idx], color="red", alpha=anomaly_proba[y_pred == -1], label="Anomaly Frequency")
-axes[1][0].set_title("Frequency of incoming messages")
-axes[1][0].legend()
-axes[1][0].grid()
+    plt.legend()
+    plt.grid()
 
-axes[1][1].scatter(data_numpy[:, time_idx], data_numpy[:, cancellation_rate_idx], color="green", label="Normal Cancellations Rate")
-axes[1][1].scatter(data_numpy[y_pred == -1, time_idx], data_numpy[y_pred == -1, cancellation_rate_idx], color="red", alpha=anomaly_proba[y_pred == -1], label="Anomaly Cancellations Rate")
-axes[1][1].set_title("Cancellations Rate")
-axes[1][1].legend()
-axes[1][1].grid()
-
-plt.show()
+    plt.savefig(f"img/anomaly_detections/{DATE}_{MARKET_SEGMENT_ID}_{SECURITY_ID}/OCSVM_{data.columns[index]}.png")
+    plt.show()
