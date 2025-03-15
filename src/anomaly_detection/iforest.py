a59c9e035136d73a8f9f4e6c5c07307ceff62e08
SpeekeR <DominikZappe1999@seznam.cz>
2025-03-15 20:23:53 +0100

    Isolation Forest evaluated on the EM/MV metrics using 5-fold Cross Validation Image of the evaluation is based on the first 10 000 timestamps only (local PC speed purposes)

--- src/anomaly_detection/iforest.py
+++ src/anomaly_detection/iforest.py
@@ -4,6 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.ensemble import IsolationForest
 from sklearn.model_selection import KFold
+from eval.eval import evaluate
 
 # Load the data
 DATE = "20191202"
@@ -23,7 +24,7 @@ except KeyError as e:
     exit(1)
 
 # Take smaller subset of the data (for local computer speed purposes)
-# data = data.head(100000)
+data = data.head(10000)
 
 # Get the indices of the columns
 time_idx = data.columns.get_loc("Time")
@@ -49,19 +50,75 @@ kf = KFold(n_splits=5)
 y_pred = np.zeros_like(data_numpy[:, 0])
 y_scores = np.zeros_like(data_numpy[:, 0])
 
+# Prepare the evaluation results
+em_vals = []
+mv_vals = []
+em_curves = []
+mv_curves = []
+t = []
+axis_alpha = []
+amax = -1
+
 # Initialize and fit the model
 model = IsolationForest(contamination=0.01)
 # model = IsolationForest(contamination=0.01, n_estimators=1000, max_samples=1.0, max_features=1.0)
 
+i = 1
 for train_index, test_index in kf.split(data_numpy):
+    print(f"KFold {i}")
+    i += 1
     # Fit the model
     model.fit(data_numpy[train_index])
     # Predict the anomalies in the data
     y_pred[test_index] = model.predict(data_numpy[test_index])
     y_scores[test_index] = model.decision_function(data_numpy[test_index])
+    # Evaluate the model
+    em_val, mv_val, em_curve, mv_curve, t_, axis_alpha_, amax_ = evaluate(model, data_numpy[train_index], data_numpy[test_index])
+    em_vals.append(em_val)
+    mv_vals.append(mv_val)
+    em_curves.append(em_curve)
+    mv_curves.append(mv_curve)
+    t = t_
+    axis_alpha = axis_alpha_
+    amax = max(amax, amax_)
 
+# Calculate the anomaly probability
 y_scores_norm = (y_scores - y_scores.min()) / (y_scores.max() - y_scores.min())
 anomaly_proba = 1 - y_scores_norm  # The lower the original score, the higher "certainty" it is an anomaly
+
+# Average the evaluation results
+em_val = np.mean(em_vals)
+mv_val = np.mean(mv_vals)
+em_curve = np.mean(em_curves, axis=0)
+mv_curve = np.mean(mv_curves, axis=0)
+
+# Print and plot the evaluation results
+print(f"EM: {em_val}")
+print(f"MV: {mv_val}")
+fig = plt.figure(figsize=(20, 10))
+fig.suptitle("Isolation Forest")
+
+plt.subplot(121)
+plt.plot(t[:amax], em_curve[:amax], lw=1, label=f'Isolation Forest (EM-score = {em_val:.3e})')
+
+plt.ylim([-0.05, 1.05])
+plt.xlabel('t')
+plt.ylabel('EM(t)')
+
+plt.title('Excess Mass (EM) curves')
+plt.legend()
+
+plt.subplot(122)
+plt.plot(axis_alpha, mv_curve, lw=1, label=f'Isolation Forest (AUC = {mv_val:.3f})')
+
+plt.xlabel('alpha')
+plt.ylabel('MV(alpha)')
+
+plt.title('Mass-Volume (MV) curves')
+plt.legend()
+
+plt.savefig(f"img/anomaly_detections/{DATE}_{MARKET_SEGMENT_ID}_{SECURITY_ID}/IF_EM_MV.png")
+plt.show()
 
 # Plot the anomalies
 for index in indcs:
