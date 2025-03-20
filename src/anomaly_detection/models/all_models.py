e0108f031443cc9d1db70ea8062ee58b824965cb
SpeekeR <DominikZappe1999@seznam.cz>
2025-03-20 23:29:44 +0100

    CNN Autoencoder fully integrated into all_models.py Image of all models evaluation also added, both Autoencoders look very bad (for now) -- they possibly need way more data and time to train properly (will be done later!)

--- src/anomaly_detection/models/all_models.py
+++ src/anomaly_detection/models/all_models.py
@@ -4,7 +4,7 @@ from sklearn.ensemble import IsolationForest
 from sklearn.svm import OneClassSVM
 from sklearn.neighbors import LocalOutlierFactor
 
-from src.anomaly_detection.models.autoencoder import FFNNAutoencoder
+from src.anomaly_detection.models.autoencoder import FFNNAutoencoder, CNNAutoencoder, create_sequences, undo_sequences
 
 from src.anomaly_detection.dataloader import load_data
 from src.anomaly_detection.training import train_model, train_torch_model
@@ -28,22 +28,26 @@ def main():
     # Transform data to PyTorch tensors and normalize the data
     data_tensor = torch.tensor(data_numpy, dtype=torch.float32)
     data_tensor = (data_tensor - data_tensor.mean(dim=0)) / data_tensor.std(dim=0)  # Normalize the data
+    seq_len = 300
+    data_tensor_seq = create_sequences(data_tensor, seq_len=seq_len)
     batch_size = 32
-    data_loader = DataLoader(data_tensor, batch_size=batch_size, shuffle=True)
+    data_loader = DataLoader(data_tensor, batch_size=batch_size)
+    data_loader_seq = DataLoader(data_tensor_seq, batch_size=batch_size)
 
     # Initialize the models
     print("Initializing the models...")
     model_if = IsolationForest(contamination=0.01)
-    # model = IsolationForest(contamination=0.01, n_estimators=1000, max_samples=1.0, max_features=1.0)
     model_ocsvm = OneClassSVM(kernel="rbf", gamma="scale")
     model_lof = LocalOutlierFactor(n_neighbors=32, contamination=0.01, novelty=True)
     latent_dimensions = 4
     model_ffnn_ae = FFNNAutoencoder(input_size=data_numpy.shape[1], latent_space_size=latent_dimensions)
+    model_cnn_ae = CNNAutoencoder(input_size=data_numpy.shape[1], latent_space_size=latent_dimensions)
 
     # Train the models
     print("Training the models...")
     num_epochs = 10
-    lr = 1e-5
+    lr_e_5 = 1e-5
+    lr_e_4 = 1e-4
     kfolds = 5
 
     print("Isolation Forest")
@@ -53,23 +57,28 @@ def main():
     print("Local Outlier Factor")
     y_pred_lof, y_scores_lof, anomaly_proba_lof, em_val_lof, mv_val_lof, em_curve_lof, mv_curve_lof, t_lof, axis_alpha_lof, amax_lof = train_model(model_lof, data_numpy, kfolds=kfolds, eval=True)
     print("FFNN Autoencoder")
-    y_pred_ffnnae, y_scores_ffnnae, anomaly_proba_ffnnae, em_val_ffnnae, mv_val_ffnnae, em_curve_ffnnae, mv_curve_ffnnae, t_ffnnae, axis_alpha_ffnnae, amax_ffnnae = train_torch_model(model_ffnn_ae, data_loader, num_epochs=num_epochs, lr=lr, kfolds=kfolds, eval=True)
+    y_pred_ffnnae, y_scores_ffnnae, anomaly_proba_ffnnae, em_val_ffnnae, mv_val_ffnnae, em_curve_ffnnae, mv_curve_ffnnae, t_ffnnae, axis_alpha_ffnnae, amax_ffnnae = train_torch_model(model_ffnn_ae, data_loader, num_epochs=num_epochs, lr=lr_e_5, kfolds=kfolds, eval=True)
+    print("CNN Autoencoder")
+    y_pred_cnnae, y_scores_cnnae, anomaly_proba_cnnae, em_val_cnnae, mv_val_cnnae, em_curve_cnnae, mv_curve_cnnae, t_cnnae, axis_alpha_cnnae, amax_cnnae = train_torch_model(model_cnn_ae, data_loader_seq, num_epochs=num_epochs, lr=lr_e_4, kfolds=kfolds, eval=True)
+
+    # Transform sequences back to original shapes
+    y_pred_cnnae, y_scores_cnnae, anomaly_proba_cnnae = undo_sequences(y_scores_cnnae, seq_len=seq_len)
 
     # Prepare data for plots
     print("Plotting the results...")
     time_idx = data.columns.get_loc("Time")
     indcs = [data.columns.get_loc(feature) for feature in WANTED_FEATURES[1:]]  # Skip the "Time" column
-    model_names = ["Isolation Forest", "One-Class SVM", "Local Outlier Factor", "FFNN Autoencoder"]
-    short_model_names = ["IF", "OCSVM", "LOF", "FFNNAE"]
-    y_preds = [y_pred_if, y_pred_ocsvm, y_pred_lof, y_pred_ffnnae]
-    anomaly_probas = [anomaly_proba_if, anomaly_proba_ocsvm, anomaly_proba_lof, anomaly_proba_ffnnae]
-    em_vals = [em_val_if, em_val_ocsvm, em_val_lof, em_val_ffnnae]
-    mv_vals = [mv_val_if, mv_val_ocsvm, mv_val_lof, mv_val_ffnnae]
-    em_curves = [em_curve_if, em_curve_ocsvm, em_curve_lof, em_curve_ffnnae]
-    mv_curves = [mv_curve_if, mv_curve_ocsvm, mv_curve_lof, mv_curve_ffnnae]
+    model_names = ["Isolation Forest", "One-Class SVM", "Local Outlier Factor", "FFNN Autoencoder", "CNN Autoencoder"]
+    short_model_names = ["IF", "OCSVM", "LOF", "FFNNAE", "CNNAE"]
+    y_preds = [y_pred_if, y_pred_ocsvm, y_pred_lof, y_pred_ffnnae, y_pred_cnnae]
+    anomaly_probas = [anomaly_proba_if, anomaly_proba_ocsvm, anomaly_proba_lof, anomaly_proba_ffnnae, anomaly_proba_cnnae]
+    em_vals = [em_val_if, em_val_ocsvm, em_val_lof, em_val_ffnnae, em_val_cnnae]
+    mv_vals = [mv_val_if, mv_val_ocsvm, mv_val_lof, mv_val_ffnnae, mv_val_cnnae]
+    em_curves = [em_curve_if, em_curve_ocsvm, em_curve_lof, em_curve_ffnnae, em_curve_cnnae]
+    mv_curves = [mv_curve_if, mv_curve_ocsvm, mv_curve_lof, mv_curve_ffnnae, mv_curve_cnnae]
     t = t_if
     axis_alpha = axis_alpha_if
-    amax = max(amax_if, amax_ocsvm, amax_lof, amax_ffnnae)
+    amax = max(amax_if, amax_ocsvm, amax_lof, amax_ffnnae, amax_cnnae)
 
     # Plot the evaluation results
     plot_eval_res(DATE, MARKET_SEGMENT_ID, SECURITY_ID, model_names, short_model_names, em_vals, mv_vals, em_curves, mv_curves, t, axis_alpha, amax)
