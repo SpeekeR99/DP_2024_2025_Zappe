da45b126ef1404705ffa2c0b0f50e305621f97e1
SpeekeR <DominikZappe1999@seznam.cz>
2025-03-24 19:00:00 +0100

    Minor fashion changes to the scripts for MetaCentrum

--- src/meta_centrum/anomaly_detection_training/scikit_training.sh
+++ src/meta_centrum/anomaly_detection_training/scikit_training.sh
@@ -6,7 +6,7 @@
 
 # Setup environment
 CONTAINER=/cvmfs/singularity.metacentrum.cz/NGC/PyTorch:25.02-py3.SIF
-#DATADIR=/storage/plzen1/home/zapped99/s3/a7/witching_days/keep/XEUR_2021_witching_days
+# DATADIR=/storage/plzen1/home/zapped99/s3/a7/witching_days/keep/XEUR_2021_witching_days
 DATADIR=/storage/plzen1/home/zapped99/dp/mock_data
 PROJECT_DIR=/storage/plzen1/home/zapped99/dp
 API_KEY=$(cat $PROJECT_DIR/.wandb_api_key)
