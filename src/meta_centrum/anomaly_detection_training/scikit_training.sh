af6d74a75bb20df877e082d4a07b6e9961b3c2d7
SpeekeR <DominikZappe1999@seznam.cz>
2025-03-24 17:09:48 +0100

    Why does Elixir even exist on MetaCentrum? Why does using MetaCentrum cause so much pain?

--- src/meta_centrum/anomaly_detection_training/scikit_training.sh
+++ src/meta_centrum/anomaly_detection_training/scikit_training.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -q default@pbs-m1.metacentrum.cz
 #PBS -l walltime=1:0:0
-#PBS -l select=1:ncpus=1:mem=32gb:scratch_local=64gb
+#PBS -l select=1:ncpus=1:mem=32gb:scratch_local=64gb:cl_elbi1=False:cl_elmu1=False:cl_eluo1=False:cl_elum1=False:cl_elwe=False:cl_elmo5=False:cl_elmo4=False:cl_eltu=False:cl_elmo3=False:cl_elmo2=False:cl_elmo1=False
 #PBS -N anomaly_detection_torch_training
 
 # Setup environment
@@ -22,18 +22,20 @@ cp -r $PROJECT_DIR/DP_2024_2025_Zappe/* .
 data_file="${market_id}_${date}_${market_segment_id}_${security_id}_detailed.zip"
 cp $DATADIR/$data_file ./data
 # Decompress the data file
-unzip $data_file -d data
+cd data
+unzip $data_file
+cd ..
 
 # Prepare the container
 singularity run $CONTAINER pip3 install -r requirements_metacentrum.txt --user
-singularity run $CONTAINER python3 -m wandb login --relogin API_KEY
+singularity run $CONTAINER python3 -m wandb login --relogin $API_KEY
 
 # Run the parsing of .json to .csv augmented lobster format
 singularity run $CONTAINER python3 $JSON_TO_LOBSTER $market_id $date $market_segment_id $security_id
 singularity run $CONTAINER python3 $AUGMENT_LOBSTER $market_id $date $market_segment_id $security_id
 
 # Run the Python script
-singularity run $CONTAINER python3 $MAIN_SCRIPT --model_type $model_type --kfolds $kfolds
+singularity run $CONTAINER python3 $MAIN_SCRIPT --market_id $market_id --date $date --market_segment_id $market_segment_id --security_id $security_id --model_type $model_type --kfolds $kfolds
 
 # Cleanup
 clean_scratch
