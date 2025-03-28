533c83ded314699f5793cd3ba2f3a96318769974
SpeekeR <DominikZappe1999@seznam.cz>
2025-03-28 16:30:54 +0100

    BugFix of the Grid Search array variables

--- src/meta_centrum/anomaly_detection_training/run_scikit.sh
+++ src/meta_centrum/anomaly_detection_training/run_scikit.sh
@@ -27,15 +27,15 @@ model_lof="lof"
 # General HP's
 kfolds=5
 # Model specific HP's
-n_estimatorss=(50, 100, 200)
+n_estimatorss=(50 100 200)
 mock_n_estimators=100
-max_sampless=("auto", "1024", "0.1")
+max_sampless=("auto" "1024" "0.1")
 mock_max_samples="auto"
 max_featuress=(0.5 0.75 1.0)
 mock_max_features=1.0
-gammas=("scale", "auto")
+gammas=("scale" "auto")
 mock_gamma="auto"
-n_neigborss=(16, 32)
+n_neigborss=(16 32)
 mock_n_neighbors=32
 
 # Isolation Forest
@@ -43,6 +43,7 @@ for n_estimators in "${n_estimatorss[@]}"; do
     for max_samples in "${max_sampless[@]}"; do
         for max_features in "${max_featuress[@]}"; do
             # Run the training on a given market, date, market_segment_id, security_id with the given model and its params
+            echo "Running Isolation Forest with n_estimators=$n_estimators, max_samples=$max_samples, max_features=$max_features"
             qsub -v market_id="$market_id",date="$date",market_segment_id="$market_segment_id",security_id="$security_id",model_type="$model_if",kfolds="$kfolds",n_estimators="$n_estimators",max_samples="$max_samples",max_features="$max_features",gamma="$mock_gamma",n_neighbors="$mock_n_neighbors" scikit_training.sh
         done
     done
@@ -51,12 +52,14 @@ done
 # One-Class SVM
 for gamma in "${gammas[@]}"; do
       # Run the training on a given market, date, market_segment_id, security_id with the given model and its params
+        echo "Running One-Class SVM with gamma=$gamma"
       qsub -v market_id="$market_id",date="$date",market_segment_id="$market_segment_id",security_id="$security_id",model_type="$model_ocsvm",kfolds="$kfolds",n_estimators="$mock_n_estimators",max_samples="$mock_max_samples",max_features="$mock_max_features",gamma="$gamma",n_neighbors="$mock_n_neighbors" scikit_training.sh
 done
 
 # Local Outlier Factor
 for n_neighbors in "${n_neigborss[@]}"; do
     # Run the training on a given market, date, market_segment_id, security_id with the given model and its params
+    echo "Running Local Outlier Factor n_neighbors=$n_neighbors"
     qsub -v market_id="$market_id",date="$date",market_segment_id="$market_segment_id",security_id="$security_id",model_type="$model_lof",kfolds="$kfolds",n_estimators="$mock_n_estimators",max_samples="$mock_max_samples",max_features="$mock_max_features",gamma="$mock_gamma",n_neighbors="$n_neighbors" scikit_training.sh
 done
 
--- src/meta_centrum/anomaly_detection_training/run_torch.sh
+++ src/meta_centrum/anomaly_detection_training/run_torch.sh
@@ -28,11 +28,11 @@ model_transformer="transformer"
 epochs=500
 kfolds=5
 # Model specific HP's
-batch_sizes=(16, 32, 64)
-lrs=(0.01 0.001, 0.0001)
-seq_lens=(32, 64, 128)  # FFNN does not have sequences
+batch_sizes=(16 32 64)
+lrs=(0.01 0.001 0.0001)
+seq_lens=(32 64 128)  # FFNN does not have sequences
 mock_seq_len=64  # For FFNN
-latent_dims=(4, 6, 8)  # Transformer does not have latent dim
+latent_dims=(4 6 8)  # Transformer does not have latent dim
 mock_latent_dim=4  # For Transformer
 
 # FFNN
@@ -40,6 +40,7 @@ for batch_size in "${batch_sizes[@]}"; do
     for lr in "${lrs[@]}"; do
         for latent_dim in "${latent_dims[@]}"; do
             # Run the training on a given market, date, market_segment_id, security_id with the given model and its params
+            echo "Running FFNN with batch_size=$batch_size, lr=$lr, latent_dim=$latent_dim"
             qsub -v market_id="$market_id",date="$date",market_segment_id="$market_segment_id",security_id="$security_id",model_type="$model_ffnn",epochs="$epochs",kfolds="$kfolds",batch_size="$batch_size",lr="$lr",seq_len="$mock_seq_len",latent_dim="$latent_dim" torch_training_gpu.sh
         done
     done
@@ -51,6 +52,7 @@ for batch_size in "${batch_sizes[@]}"; do
         for latent_dim in "${latent_dims[@]}"; do
             for seq_len in "${seq_lens[@]}"; do
                 # Run the training on a given market, date, market_segment_id, security_id with the given model and its params
+                echo "Running CNN with batch_size=$batch_size, lr=$lr, latent_dim=$latent_dim, seq_len=$seq_len"
                 qsub -v market_id="$market_id",date="$date",market_segment_id="$market_segment_id",security_id="$security_id",model_type="$model_cnn",epochs="$epochs",kfolds="$kfolds",batch_size="$batch_size",lr="$lr",seq_len="$seq_len",latent_dim="$latent_dim" torch_training_gpu.sh
             done
         done
@@ -62,6 +64,7 @@ for batch_size in "${batch_sizes[@]}"; do
     for lr in "${lrs[@]}"; do
         for seq_len in "${seq_lens[@]}"; do
             # Run the training on a given market, date, market_segment_id, security_id with the given model and its params
+            echo "Running Transformer with batch_size=$batch_size, lr=$lr, seq_len=$seq_len"
             qsub -v market_id="$market_id",date="$date",market_segment_id="$market_segment_id",security_id="$security_id",model_type="$model_transformer",epochs="$epochs",kfolds="$kfolds",batch_size="$batch_size",lr="$lr",seq_len="$seq_len",latent_dim="$mock_latent_dim" torch_training_gpu.sh
         done
     done
