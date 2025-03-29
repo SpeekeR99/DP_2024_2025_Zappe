87eb1485324e7f7f718e82c9b1aeafb22fda9376
SpeekeR <DominikZappe1999@seznam.cz>
2025-03-29 23:04:07 +0100

    Reduction of input dimensions prepared

--- src/anomaly_detection/data/dataloader.py
+++ src/anomaly_detection/data/dataloader.py
@@ -1,5 +1,7 @@
 import os
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 
 
 def load_data(date="20191202", market_segment_id="688", security_id="4128839", relevant_features=None):
@@ -44,7 +46,7 @@ def load_data_reduced_dimensions(date="20191202", market_segment_id="688", secur
     :return: Data
     """
     # Load the data
-    data = load_data(date, market_segment_id, security_id, relevant_features)
+    data = load_data(date, market_segment_id, security_id, relevant_features).dropna()
 
     # Reduce the dimensions based on correlation and feature importance analysis
     # Keep "Time" of course
@@ -53,7 +55,26 @@ def load_data_reduced_dimensions(date="20191202", market_segment_id="688", secur
     # Throw away "Frequency of Incoming Messages" and only keep "Cancellations Rate"
     # Combine "High Quoting Activity", "Unbalanced Quoting", and "Low Execution Probability" into one component using PCA
     # Combine "Trades Oppose Quotes" and "Cancels Oppose Trades" into one component using PCA
+    data_dim_reduced = data[["Time"]].copy()
 
-    # TODO: Reduce the dimensions here
+    mid_price = (data["Ask Price 1"] + data["Bid Price 1"]) / 2.0
+    data_dim_reduced["Mid Price"] = mid_price
 
-    return data
+    pca = PCA(n_components=1)
+    imbal_data = data[["Imbalance Index", "Bid Volume 1", "Ask Volume 1"]].copy()
+    imbal_volumes_pc1 = pca.fit_transform(StandardScaler().fit_transform(imbal_data))
+    data_dim_reduced["Imbalance Volumes PC1"] = imbal_volumes_pc1
+
+    data_dim_reduced["Cancellations Rate"] = data["Cancellations Rate"]
+
+    pca = PCA(n_components=1)
+    quoting_data = data[["High Quoting Activity", "Unbalanced Quoting", "Low Execution Probability"]].copy()
+    quoting_pc1 = pca.fit_transform(StandardScaler().fit_transform(quoting_data))
+    data_dim_reduced["Quoting PC1"] = quoting_pc1
+
+    pca = PCA(n_components=1)
+    oppose_data = data[["Trades Oppose Quotes", "Cancels Oppose Trades"]].copy()
+    oppose_pc1 = pca.fit_transform(StandardScaler().fit_transform(oppose_data))
+    data_dim_reduced["Oppose PC1"] = oppose_pc1
+
+    return data_dim_reduced
